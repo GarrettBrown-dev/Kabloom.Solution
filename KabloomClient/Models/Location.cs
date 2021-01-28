5696bf463b4ddd512b51dd03fa0d27fd300df642
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-01-28 15:07:10 -0800

    try to update repo so all files in gitignore can not be seen in repo

--- KabloomClient/Models/Location.cs
+++ KabloomClient/Models/Location.cs
@@ -19,7 +19,7 @@ namespace KabloomClient.Models
     public Location Location { get; set; }
     public static List<LocationGroup> GetVenues()
     {
-      var searchLocation ="Seattle, WA";
+      var searchLocation ="Madrid, Spain";
       var apiCallTask = ApiHelper.FourSquareCall(searchLocation);
       var result = apiCallTask.Result;
 
